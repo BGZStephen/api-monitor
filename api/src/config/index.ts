2409218158137176baf017c49291d61d353bf385
Stephen <sjw948@gmail.com>
2023-12-09 14:45:04 +0000

    make config use env vars

---
+++ api/src/config/index.ts
@@ -0,0 +1,9 @@
+import { parseIntOrThrow } from "../utils/parse-int-or-throw";
+
+interface Config {
+  PORT: number;
+}
+
+export const config: Config = {
+  PORT: parseIntOrThrow(process.env["PORT"])
+}
---
+++ api/src/config/index.unit.test.ts
@@ -0,0 +1,15 @@
+import { parseIntOrThrow } from "../utils/parse-int-or-throw";
+
+jest.mock("../utils/parse-int-or-throw");
+
+describe("config", () => {
+  test("config returns an object with defaults", () => {
+    jest.mocked(parseIntOrThrow).mockReturnValue(3000);
+
+    const { config } = require(".");
+
+    expect(config).toEqual({
+      PORT: 3000
+    })
+  })
+})
