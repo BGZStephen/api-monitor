a33ce07d6d479b068be36f33cb04e4e03616d9c0
Stephen <sjw948@gmail.com>
2023-12-09 14:01:19 +0000

    add nodemon

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,3 +1,11 @@
-export function main(): void {
-  console.log("Hello, world");
-}
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(3000, () => {
+  console.log(`App listening on port 3000`)
+})
